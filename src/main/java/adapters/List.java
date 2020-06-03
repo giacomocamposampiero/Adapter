520e50e8abb6725268113a9735dbe2a2a2201094
giacomocamposampiero <giacomo.camposampiero@hotmail.com>
2020-06-03 12:05:45 +0200

    Interfaces are okay now.

--- src/main/java/adapters/List.java
+++ src/main/java/adapters/List.java
@@ -11,8 +11,8 @@ import java.util.Vector;
 
 /**
  * Implementation of List interface Java 1.4.2.
- *
- * @author Giacomo Camposampiero
+ * This implementation is the actual concrete implementation for Collection interface too. If a concrete implementation of a Collection is needed, a List object should be instantiated.
+ * @author Giacomo Camposampiero (Set interface is defined by java.util.Set and this is just an adapted redefinition of it, all credit goes to Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA)
  */
 public class List implements HList {
 
@@ -22,6 +22,14 @@ public class List implements HList {
         vec = new Vector();
     }
 
+    /**
+     * Inserts the specified element at the specified position in this list.
+     * Shifts the element currently at that position (if any) and any subsequent elements to the right (adds one to their indices). 
+     * @param index index at which the specified element is to be inserted.
+     * @param element element to be inserted. 
+     * @throws IndexOutOfBoundsException if the index parameter isn't in the interval [0, size()]
+     * @throws NullPointerException if the element parameter is null
+     */    
     @Override
     public void add(int index, Object element) {
         if (element == null) {
@@ -33,17 +41,40 @@ public class List implements HList {
         vec.insertElementAt(element, index);
     }
 
+    /**
+     * Returns true if this list changed as a result of the call. (Returns false if this list does not permit duplicates and already contains the specified element.). 
+     * @param o  element that has to be inserted. 
+     * @return  true if this list changed as a result of the call  
+     * @throws NullPointerException whether the parameter is a null reference, which is not considered a valid type of entry for the collection
+     */
     @Override
     public boolean add(Object o) {
         add(size(), o);
         return true;
     }
 
+    /**
+     * Adds all of the elements in the specified collection to this list. 
+     * The behavior of this operation is undefined if the specified collection is modified while the operation is in progress.
+     * @param c  elements to be inserted into this list.
+     * @return  true if this collection changed as a result of the call. 
+     */
     @Override
     public boolean addAll(HCollection c) {
          return addAll(size(), c);
     }
 
+    /**
+     * Inserts all of the elements in the specified collection into this list at the specified position.
+     * Shifts the element currently at that position (if any) and any subsequent elements to the right (increases their indices).
+     * The new elements will appear in this list in the order that they are returned by the specified collection's iterator.
+     * The behavior of this operation is unspecified if the specified collection is modified while the operation is in progress.
+     * @param index  index at which to insert first element from the specified collection.
+     * @param c  elements to be inserted into this list.    
+     * @return  true if this list changed as a result of the call. 
+     * @throws IndexOutOfBoundsException if the index parameter isn't in the interval [0, size()]
+     * @throws NullPointerException if the element parameter is null
+     */
     @Override
     public boolean addAll(int index, HCollection c) {
         if (index < 0 || index > size()) {
@@ -57,12 +88,23 @@ public class List implements HList {
         }
         return res;
     }
-
+    
+    /**
+     * Removes all of the elements from this list.
+     * This list will be empty after this method returns unless it throws an exception. 
+     */
     @Override
     public void clear() {
         vec.removeAllElements();
     }
 
+    /**
+     * Returns true if this list contains the specified element.
+     * More formally, returns true if and only if this list contains at least one element e such that (o==null ? e==null : o.equals(e)). 
+     * @param o  element whose presence in this list is to be tested. 
+     * @return  true if this list contains the specified element. 
+     * @throws NullPointerException if the parameters is null.
+     */
     @Override
     public boolean contains(Object o) {
         if (o == null) {
@@ -71,6 +113,13 @@ public class List implements HList {
         return vec.contains(o);
     }
 
+    /**
+     * Returns true if this list contains all of the elements of the specified collection.
+     * @param c  collection to be checked for containment in this list. 
+     * @return  true if this list contains all of the elements of the specified collection. 
+     * @throws NullPointerException if the parameters is null.
+     * @throws NullPointerException if the specified collection contains one or more null elements.
+     */
     @Override
     public boolean containsAll(HCollection c) {
         boolean res = true;
@@ -81,6 +130,12 @@ public class List implements HList {
         return res;
     }
 
+    /**
+     * Compares the specified object with this list for equality.
+     * The general contract for the Object.equals method states that equals must be symmetric (in other words, a.equals(b) if and only if b.equals(a)). 
+     * @param o  the object to be compared for equality with this list. 
+     * @return  true if the specified object is equal to this list.
+     */
     @Override
     public boolean equals(Object o) {
         if (o == this) {
@@ -101,6 +156,11 @@ public class List implements HList {
         return true;
     }
 
+    /**
+     * Returns the hash code value for this list.
+     * In particular, c1.equals(c2) implies that c1.hashCode()==c2.hashCode(). 
+     * @return the hash code value for this list.
+     */
     @Override
     public int hashCode() {
         int hashCode = 1;
@@ -112,6 +172,12 @@ public class List implements HList {
         return hashCode;
     }
 
+    /**
+     * Returns the element at the specified position in this list.
+     * @param index index of element to return. 
+     * @return  the element at the specified position in this list. 
+     * @throws IndexOutOfBoundsException if the index parameter isn't in the interval [0, size()-1]
+     */
     @Override
     public Object get(int index) {
         if (index < 0 || index >= size()) {
@@ -120,6 +186,13 @@ public class List implements HList {
         return vec.elementAt(index);
     }
 
+    /**
+     * Returns the index in this list of the first occurrence of the specified element, or -1 if this list does not contain this element.
+     * More formally, returns the lowest index i such that (o==null ? get(i)==null : o.equals(get(i))), or -1 if there is no such index. 
+     * @param o  element to search for.     
+     * @return  the index in this list of the first occurrence of the specified element, or -1 if this list does not contain this element. 
+     * @throws NullPointerException if the element parameter is null
+     */
     @Override
     public int indexOf(Object o) {
         if (o == null) {
@@ -128,16 +201,31 @@ public class List implements HList {
         return vec.indexOf(o);
     }
 
+    /**
+     * Returns true if this list contains no elements.
+     * @return  true if this list contains no elements.
+     */
     @Override
     public boolean isEmpty() {
         return vec.isEmpty();
     }
 
+    /**
+     * Returns an iterator over the elements in this list in proper sequence. 
+     * @return  an iterator over the elements in this collection.
+     */
     @Override
     public HIterator iterator() {
         return new ListIterator(0, size());
     }
 
+    /**
+     * Returns the index in this list of the last occurrence of the specified element, or -1 if this list does not contain this element.
+     * More formally, returns the highest index i such that (o==null ? get(i)==null : o.equals(get(i))), or -1 if there is no such index. 
+     * @param o  element to search for. 
+     * @return the index in this list of the last occurrence of the specified element, or -1 if this list does not contain this element. 
+     * @throws NullPointerException if the element parameter is null
+     */
     @Override
     public int lastIndexOf(Object o) {
         if (o == null) {
@@ -146,11 +234,20 @@ public class List implements HList {
         return vec.lastIndexOf(o);
     }
 
+    /**
+     * Returns a list iterator of the elements in this list (in proper sequence).
+     * @return a list iterator of the elements in this list (in proper sequence).
+     */
     @Override
     public HListIterator listIterator() {
         return listIterator(0);
     }
 
+    /**
+     * Returns a list iterator of the elements in this list (in proper sequence), starting at the specified position in this list.
+     * @param index  index of first element to be returned from the list iterator (by a call to the next method). 
+     * @return  a list iterator of the elements in this list (in proper sequence), starting at the specified position in this list. 
+     */
     @Override
     public HListIterator listIterator(int index) {
         if (index < 0 || index >= size()) {
@@ -159,6 +256,14 @@ public class List implements HList {
         return new ListIterator(index, size());
     }
 
+    /**
+     * Removes the element at the specified position in this list.
+     * Shifts any subsequent elements to the left (subtracts one from their indices). Returns the element that was removed from the list. 
+     * @param index the index of the element to removed.   
+     * @return the element previously at the specified position.
+     * @throws IndexOutOfBoundsException if the index parameter isn't in the interval [0, size()]
+     * @throws NullPointerException if the element parameter is null
+     */
     @Override
     public Object remove(int index) {
         if (index < 0 || index >= size()) {
@@ -169,6 +274,13 @@ public class List implements HList {
         return old;
     }
 
+    /**
+     * Removes the first occurrence in this list of the specified element, if it is present.
+     * More formally, removes an element e such that (o==null ? e==null : o.equals(e)), if this list contains one or more such elements. Returns true if this list contained the specified element (or equivalently, if this list changed as a result of the call). 
+     * @param o  element to be removed from this list, if present. 
+     * @return  true if this list contained the specified element. 
+     * @throws NullPointerException if the parameters is null.
+     */
     @Override
     public boolean remove(Object o) {
         if (o == null) {
@@ -177,6 +289,14 @@ public class List implements HList {
         return vec.remove(o);
     }
 
+    /**
+     * Removes from this list all the elements that are contained in the specified collection.
+     * After this call returns, this list will contain no elements in common with the specified collection. 
+     * @param c  collection that defines which elements will be removed from this list.  
+     * @return  true if this list changed as a result of the call. 
+     * @throws NullPointerException if the parameters is null.
+     * @throws NullPointerException if the specified collection contains one or more null elements.
+     */
     @Override
     public boolean removeAll(HCollection c) {
         boolean changed = false;
@@ -186,6 +306,14 @@ public class List implements HList {
         return changed;
     }
 
+    /**
+     * Retains only the elements in this list that are contained in the specified collection.
+     * In other words, removes from this list all of its elements that are not contained in the specified collection.  
+     * @param c  collection that defines which elements this list will retain. 
+     * @return  true if this list changed as a result of the call. 
+     * @throws NullPointerException if the parameters is null.
+     * @throws NullPointerException if the specified collection contains one or more null elements.
+     */
     @Override
     public boolean retainAll(HCollection c) {
         boolean changed = false;
@@ -197,6 +325,14 @@ public class List implements HList {
         return changed;   
     }
 
+    /**
+     * Replaces the element at the specified position in this list with the specified element.
+     * @param index  index of element to replace.
+     * @param element element to be stored at the specified position. 
+     * @return the element previously at the specified position. 
+     * @throws IndexOutOfBoundsException if the index parameter isn't in the interval [0, size()-1]
+     * @throws NullPointerException if the element parameter is null
+     */
     @Override
     public Object set(int index, Object element) {
         if (element == null) {
@@ -210,17 +346,32 @@ public class List implements HList {
         return old;
     }
 
+    /**
+     * Returns the number of elements in this collection.
+     * @return the number of elements in this collection.
+     */
     @Override
     public int size() {
         return vec.size();
     }
 
+    /**
+     * Returns a view of the portion of this list between the specified fromIndex, inclusive, and toIndex, exclusive.
+     * @param fromIndex  low endpoint (inclusive) of the subList.
+     * @param toIndex  high endpoint (exclusive) of the subList. 
+     * @return  a view of the specified range within this list. 
+     */
     @Override
     public HList subList(int fromIndex, int toIndex) {
         if(fromIndex < 0 || fromIndex > toIndex || toIndex >= size()) throw new IndexOutOfBoundsException();
         return new SubList(fromIndex, toIndex);
     }
 
+    /**
+     * Returns an array containing all of the elements in this list.
+     * The returned array will be "safe" in that no references to it are maintained by this list. (In other words, this method must allocate a new array even if this list is backed by an array). The caller is thus free to modify the returned array.
+     * @return  an array containing all of the elements in this list.
+     */
     @Override
     public Object[] toArray() {
         Object[] res = new Object[size()];
--- src/main/java/adapters/Set.java
+++ src/main/java/adapters/Set.java
@@ -25,7 +25,14 @@ public class Set implements HSet{
         this.table = new Hashtable(initialCapacity);
     }
     
-    
+    /**
+     * Adds the specified element to this set if it is not already present.
+     * More formally, adds the specified element, o, to this set if this set contains no element e such that (o==null ? e==null : o.equals(e)). If this set already contains the specified element, the call leaves this set unchanged and returns false. In combination with the restriction on constructors, this ensures that sets never contain duplicate elements.
+     * null is not a valid value in then set, so the attempt of adding a null to the set will result in the throwing of an un-checked exception
+     * @param o  element to be appended to this set. 
+     * @return true if this set did not already contain the specified element.  
+     * @throws NullPointerException if the specified element is null.
+     */
     @Override
     public boolean add(Object o) {
         if(table.containsKey(o.hashCode())) return false;
@@ -33,6 +40,14 @@ public class Set implements HSet{
         return true;
     }
 
+    /**
+     * Adds all of the elements in the specified collection to this set if they're not already present.
+     * If the specified collection is also a set, the addAll operation effectively modifies this set so that its value is the union of the two sets. The behavior of this operation is unspecified if the specified collection is modified while the operation is in progress. 
+     * @param c  collection whose elements are to be added to this set. 
+     * @return true if this set changed as a result of the call.
+     * @throws NullPointerException if the specified element is null.
+     * @throws NullPointerException if the specified collection contains an element which is null.
+     */
     @Override
     public boolean addAll(HCollection c) {
         HIterator it = c.iterator();
@@ -43,11 +58,22 @@ public class Set implements HSet{
         return res;
     }
 
+    /**
+     * Removes all of the elements from this set.
+     * This set will be empty after this call returns. 
+     */
     @Override
     public void clear() {
         table.clear();
     }
 
+    /**
+     * Returns true if this set contains the specified element.
+     * More formally, returns true if and only if this set contains an element e such that (o==null ? e==null : o.equals(e)). 
+     * @param o  element whose presence in this set is to be tested. 
+     * @return  true if this set contains the specified element. 
+     * @throws NullPointerException if the specified element is null.
+     */
     @Override
     public boolean contains(Object o) {
         int hash = o.hashCode();
@@ -55,6 +81,14 @@ public class Set implements HSet{
         return o.equals(table.get(hash));
     }
 
+    /**
+     * Returns true if this set contains all of the elements of the specified collection. 
+     * If the specified collection is also a set, this method returns true if it is a subset of this set.
+     * @param c  collection to be checked for containment in this set. 
+     * @return  true if this set contains all of the elements of the specified collection.
+     * @throws NullPointerException if the specified element is null.
+     * @throws NullPointerException if the specified collection contains an element which is null.
+     */
     @Override
     public boolean containsAll(HCollection c) {
         HIterator it = c.iterator();
@@ -65,63 +99,12 @@ public class Set implements HSet{
         return res;
     }
 
-    @Override
-    public boolean isEmpty() {
-        return table.isEmpty();
-    }
-
-    @Override
-    public HIterator iterator() {
-        return new SetIterator();
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        if(!contains(o)) return false;
-        table.remove(o.hashCode());
-        return true;
-    }
-
-    @Override
-    public boolean removeAll(HCollection c) {
-        HIterator it = c.iterator();
-        boolean res = false;
-        while(it.hasNext()){
-            res = remove(it.next()) || res;
-        }
-        return res;
-    }
-
-    @Override
-    public boolean retainAll(HCollection c) {
-        boolean res = false;
-        HIterator it = iterator();
-        while(it.hasNext()) {
-            Object elem = it.next();
-            if(!c.contains(elem)) res = remove(elem) || res;
-        }
-        return res;
-    }
-    
-    @Override
-    public int size() {
-        return table.size();
-    }
-
-    @Override
-    public Object[] toArray() {
-        Object[] res = new Object[size()];
-        HIterator it = iterator();
-        int i = 0;
-        while(it.hasNext()) res[i++] = it.next();
-        return res;
-    }
-
-    @Override
-    public Object[] toArray(Object[] a) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+    /**
+     * Compares the specified object with this set for equality.
+     * Returns true if the specified object is also a set, the two sets have the same size, and every member of the specified set is contained in this set (or equivalently, every member of this set is contained in the specified set). This definition ensures that the equals method works properly across different implementations of the set interface. 
+     * @param o  the object to be compared for equality with this set. 
+     * @return  true if the specified object is equal to this set.
+     */
     @Override 
     public boolean equals(Object o) {
          if (o == this) return true; 
@@ -136,6 +119,11 @@ public class Set implements HSet{
         return true;
     }
     
+    /**
+     * Returns the hash code value for this set.
+     * The hash code of a set is defined to be the sum of the hash codes of the elements in the set, where the hashcode of a null element is defined to be zero. This ensures that s1.equals(s2) implies that s1.hashCode()==s2.hashCode() for any two sets s1 and s2, as required by the general contract of the Object.hashCode method. 
+     * @return  the hash code value for this set.
+     */
     @Override 
     public int hashCode() {
         int hash = 0;
@@ -143,6 +131,103 @@ public class Set implements HSet{
             hash += elems.nextElement().hashCode();
         }
         return hash;
+    }
+    
+    /**
+     * Returns true if this set contains no elements. 
+     * @return  true if this set contains no elements.
+     */
+    @Override
+    public boolean isEmpty() {
+        return table.isEmpty();
+    }
+
+    /**
+     * Returns an iterator over the elements in this set.
+     * The elements are returned in no particular order.
+     * @return an iterator over the elements in this set.
+     */
+    @Override
+    public HIterator iterator() {
+        return new SetIterator();
+    }
+
+    /**
+     * Removes the specified element from this set if it is present.
+     * More formally, removes an element e such that (o==null ? e==null : o.equals(e)), if the set contains such an element. Returns true if the set contained the specified element (or equivalently, if the set changed as a result of the call). (The set will not contain the specified element once the call returns.) 
+     * @param o  element to be removed from this set, if present. 
+     * @return  true if this set contained the specified element. 
+     * @throws NullPointerException if the specified element is null.
+     */
+    @Override
+    public boolean remove(Object o) {
+        if(!contains(o)) return false;
+        table.remove(o.hashCode());
+        return true;
+    }
+
+    /**
+     * Removes from this set all of its elements that are contained in the specified collection.
+     * If the specified collection is also a set, this operation effectively modifies this set so that its value is the asymmetric set difference of the two sets. 
+     * @param c  collection that defines which elements will be removed from this set.  
+     * @return  true if this set changed as a result of the call. 
+     * @throws NullPointerException if the specified element is null.
+     * @throws NullPointerException if the specified collection contains an element which is null.
+     */
+    @Override
+    public boolean removeAll(HCollection c) {
+        HIterator it = c.iterator();
+        boolean res = false;
+        while(it.hasNext()){
+            res = remove(it.next()) || res;
+        }
+        return res;
+    }
+
+    /**
+     * Retains only the elements in this set that are contained in the specified collection.
+     * In other words, removes from this set all of its elements that are not contained in the specified collection. If the specified collection is also a set, this operation effectively modifies this set so that its value is the intersection of the two sets.
+     * @param c  collection that defines which elements this set will retain. 
+     * @return  true if this collection changed as a result of the call. 
+     * @throws NullPointerException if the specified element is null.
+     * @throws NullPointerException if the specified collection contains an element which is null.
+     */
+    @Override
+    public boolean retainAll(HCollection c) {
+        boolean res = false;
+        HIterator it = iterator();
+        while(it.hasNext()) {
+            Object elem = it.next();
+            if(!c.contains(elem)) res = remove(elem) || res;
+        }
+        return res;
+    }
+    
+    /**
+     * Returns the number of elements in this set (its cardinality). 
+     * @return the number of elements in this set.
+     */
+    @Override
+    public int size() {
+        return table.size();
+    }
+
+    /**
+     * Returns an array containing all of the elements in this set.
+     * @return  an array containing all of the elements in this set.
+     */
+    @Override
+    public Object[] toArray() {
+        Object[] res = new Object[size()];
+        HIterator it = iterator();
+        int i = 0;
+        while(it.hasNext()) res[i++] = it.next();
+        return res;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     class SetIterator implements HIterator {
